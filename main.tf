42438f02d651fc17e692ff56e9540f6a3acb6b9a
Ali <ali@infracost.io>
2022-08-30 19:46:36 -0700

    Revert test

--- main.tf
+++ main.tf
@@ -6,14 +6,12 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
-variable "vol_size" {}
-
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   root_block_device {
-    volume_size = var.vol_size
+    volume_size = 10
   }
 
   ebs_block_device {
