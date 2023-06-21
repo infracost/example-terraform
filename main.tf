0c3160a97c067b5dd328f19e69bd08e701e6c078
Duncan MacQuarrie <5956299+3dbrows@users.noreply.github.com>
2023-06-21 16:47:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
-
+  count = 1
   root_block_device {
     volume_size = 50
   }
