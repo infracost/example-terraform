b000e5df43c71e4ad8b4ca2b53f79680f0f81392
Ali Khajeh-Hosseini <ali@infracost.io>
2023-09-18 11:48:33 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,7 @@ resource "aws_instance" "web_app" {
   }
 }
 
+# API
 resource "aws_lambda_function" "hello_world" {
   function_name = "hello_world"
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
