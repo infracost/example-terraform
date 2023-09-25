e9ab06e6ea749242dda3957d5a3ec0f03087424f
Ali <ali@infracost.io>
2023-09-25 18:39:36 +0300

    feat: add tags

--- main.tf
+++ main.tf
@@ -10,6 +10,11 @@ resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
   instance_type = "m5.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
+
   root_block_device {
     volume_size = 50
   }
@@ -28,4 +33,11 @@ resource "aws_lambda_function" "hello_world" {
   handler       = "exports.test"
   runtime       = "nodejs12.x"
   memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+
+  tags = {
+    Environment = "Prod"
+    Service = "api"
+  }
 }
+
+
