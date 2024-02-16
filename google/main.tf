0be73736e57e1907f7ac4cc4ddd608bb831b2bbe
Ali <ali@infracost.io>
2024-02-16 16:49:57 +0300

    Update examples

--- google/main.tf
+++ google/main.tf
@@ -1,32 +1,45 @@
 provider "google" {
- region      = "us-central1"
+  region  = "us-central1"
+  project = "test"
 }
 
 resource "google_compute_instance" "my_instance" {
-  zone         = "us-central1-a"
+  zone = "us-central1-a"
+  name = "test"
 
-  machine_type = "n1-standard-16" # <<<<< Try changing this to n1-standard-32 to compare the costs
+  machine_type = "n1-standard-16" # <<<<<<<<<< Try changing this to n1-standard-32 to compare the costs
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
 
   scheduling {
     preemptible = true
   }
 
   guest_accelerator {
-    type = "nvidia-tesla-t4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
+    type  = "nvidia-tesla-t4" # <<<<<<<<<< Try changing this to nvidia-tesla-p4 to compare the costs
     count = 4
   }
 
   labels = {
     Environment = "production"
-    Service = "web-app"
+    Service     = "web-app"
   }
 }
 
 resource "google_cloudfunctions_function" "my_function" {
-  runtime = "nodejs10"
+  runtime             = "nodejs20"
+  name                = "test"
   available_memory_mb = 512
 
   labels = {
-    Environment = "Prod"
+    environment = "Prod"
   }
 }
