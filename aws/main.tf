3a1a8c86d75c32f70676ef005354578bcd945018
Jack Finkel <jack.finkel@datadoghq.com>
2024-08-19 10:43:21 -0400

    alter ec2 instance type and volume type

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "my_web_app" {
   ami           = "ami-005e54dee72cc1d00"
 
-  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+  instance_type = "m7g.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
 
   tags = {
     Environment = "production"
@@ -18,6 +18,7 @@ resource "aws_instance" "my_web_app" {
 
   root_block_device {
     volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+    volume_type = gp3  
   }
 }
 
