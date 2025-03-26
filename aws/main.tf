52311c0cbd23a9880431c72f62c53f00f6f9883a
krishnakokila1990 <128783316+krishnakokila1990@users.noreply.github.com>
2025-03-26 11:49:37 +0530

    Update main.tf

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "my_web_app" {
   ami           = "ami-005e54dee72cc1d00"
 
-  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+  instance_type = "m5.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
 
   tags = {
     Environment = "production"
@@ -17,6 +17,7 @@ resource "aws_instance" "my_web_app" {
   }
 
   root_block_device {
+    volume_type="gp3"
     volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
   }
 }
