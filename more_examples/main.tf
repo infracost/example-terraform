d640035af300d500b4e1afc41a15b0efd9c2ca19
Ali <ali@infracost.io>
2025-05-20 14:27:42 +0200

    feat: add resource

--- more_examples/main.tf
+++ more_examples/main.tf
@@ -6,34 +6,6 @@ provider "aws" {
   secret_key                  = "mock_secret_key"
 }
 
-resource "aws_instance" "my_web_app" {
-  ami           = "ami-005e54dee72cc1d00"
-
-  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
-
-  tags = {
-    Environment = "production"
-    Service     = "web-app"
-  }
-
-  root_block_device {
-    volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
-  }
-}
-
-resource "aws_lambda_function" "my_hello_world" {
-  runtime       = "nodejs12.x"
-  handler       = "exports.test"
-  image_uri     = "test"
-  function_name = "test"
-  role          = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
-
-  memory_size = 512
-  tags = {
-    Environment = "Prod"
-  }
-}
-
 resource "aws_cloudwatch_log_group" "app_logs" {
   name = "my_app_logs"
 }
@@ -85,4 +57,16 @@ module "eks" {
       instance_types = ["c5.2xlarge"]
     }
   }
+}
+
+provider "azurerm" {
+  skip_provider_registration = true
+  features {}
+}
+
+resource "azurerm_log_analytics_workspace" "azure_logs" {
+  name                = "example-logs"
+  location            = "East US" 
+  resource_group_name = "example-rg"
+  sku                 = "PerGB2018"
 }
