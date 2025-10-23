8f15d2dead58f18ec7022877731dacc46c6cd23a
Sajjad Amiri <sajjad.amiri@joinroot.com>
2025-10-23 10:20:00 -0500

    fix: update instance type for my_web_app to m5.8xlarge and adjust allocated storage to 1000

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "my_web_app" {
   ami = "ami-005e54dee72cc1d00"
 
-  instance_type = "m5.16xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+  instance_type = "m5.8xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
 
   tags = {
     Environment = "production"
@@ -36,7 +36,7 @@ resource "aws_lambda_function" "my_hello_world" {
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+  allocated_storage    = 1000
   db_name              = "mydb"
   engine               = "mysql"
   engine_version       = "8.0"
