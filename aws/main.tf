79e8d85dc99c7d225af676147201193d116851df
Sajjad Amiri <sajjad.amiri@joinroot.com>
2025-10-24 07:48:22 -0500

    fix: revert instance type for my_web_app to m3.xlarge

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "my_web_app" {
   ami = "ami-005e54dee72cc1d00"
 
-  instance_type = "m5.8xlarge"
+  instance_type = "m3.xlarge"
 
   tags = {
     Environment = "production"
