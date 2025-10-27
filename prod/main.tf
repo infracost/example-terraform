c43a0b90fe588e2dd611bf90207292b0ab7530a1
Ali <ali@infracost.io>
2025-10-27 12:52:19 -0700

    Test two projects

---
+++ prod/main.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "my_web_app" {
+  ami = "ami-005e54dee72cc1d00"
+
+  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+
+  tags = {
+    Environment = "production"
+    Service     = "web-app"
+    Name        = "dash"
+  }
+
+  root_block_device {
+    volume_size = 9000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+  }
+}
