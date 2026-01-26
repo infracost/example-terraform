b4281739cb6cb7cdbaf28b64fb6f79ee1b7bc2b3
Ioannis Kolaxis <ikolaxis@gmail.com>
2026-01-26 17:50:27 +0200

    Changed EC2 instance

--- aws/main.tf
+++ aws/main.tf
@@ -9,7 +9,7 @@ provider "aws" {
 resource "aws_instance" "my_web_app" {
   ami = "ami-005e54dee72cc1d00"
 
-  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+  instance_type = "m5.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
 
   tags = {
     Environment = "production"
